b875c404e83bbb8d53d4022d025f788b9fa5e07c
Filip Engberg <filip.engberg@hiberworld.com>
2021-09-27 20:47:58 +0200

    Run `ma_context_uninit` when exiting game
    -Fixes crash when exiting game

--- src/backend/miniaudio/soloud_miniaudio.cpp
+++ src/backend/miniaudio/soloud_miniaudio.cpp
@@ -47,6 +47,7 @@ namespace SoLoud
 namespace SoLoud
 {
     ma_device gDevice;
+    ma_context gContext;
 
     void soloud_miniaudio_audiomixer(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
     {
@@ -57,14 +58,14 @@ namespace SoLoud
     static void soloud_miniaudio_deinit(SoLoud::Soloud *aSoloud)
     {
         ma_device_uninit(&gDevice);
+        ma_context_uninit(&gContext);
     }
 
     result miniaudio_init(SoLoud::Soloud *aSoloud, unsigned int aFlags, unsigned int aSamplerate, unsigned int aBuffer, unsigned int aChannels)
     {
-		ma_context context;
-		ma_context_config context_config = ma_context_config_init();
-		context_config.coreaudio.sessionCategory = ma_ios_session_category_solo_ambient;
-		ma_context_init(nullptr, 0, &context_config, &context);
+        ma_context_config context_config = ma_context_config_init();
+        context_config.coreaudio.sessionCategory = ma_ios_session_category_solo_ambient;
+        ma_context_init(nullptr, 0, &context_config, &gContext);
 
         ma_device_config config = ma_device_config_init(ma_device_type_playback);
         config.periodSizeInFrames = gDevice.playback.internalPeriodSizeInFrames;
@@ -74,7 +75,7 @@ namespace SoLoud
         config.dataCallback       = soloud_miniaudio_audiomixer;
         config.pUserData          = (void *)aSoloud;
 
-        if (ma_device_init(&context, &config, &gDevice) != MA_SUCCESS)
+        if (ma_device_init(&gContext, &config, &gDevice) != MA_SUCCESS)
         {
             return UNKNOWN_ERROR;
         }
